d1c1808e6ba4f4f4b6577996b93922a3451d917d
Mtnyazar <94050730+Mtnyazar@users.noreply.github.com>
2024-11-05 19:23:35 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductStock { get; set; }
+
+        public string ProductPrice { get; set; }
+
+        public string ProductDescription { get; set; }
+
+
+    }
+}
