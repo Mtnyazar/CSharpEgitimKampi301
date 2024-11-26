f8db7c2d7e51fab1fd348b19ede0b16fcdf8aa82
Mtnyazar <94050730+Mtnyazar@users.noreply.github.com>
2024-11-26 16:01:41 +0300

    ...

--- CSharpEgitimKampi301.EFProject/Form1.cs
+++ CSharpEgitimKampi301.EFProject/Form1.cs
@@ -16,5 +16,49 @@ namespace CSharpEgitimKampi301.EFProject
         {
             InitializeComponent();
         }
+
+        EgitimKampiEfTravelDbEntities db = new EgitimKampiEfTravelDbEntities();
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            
+            var values=db.Guide.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Guide guide=new Guide();
+            guide.GuideName=txtName.Text;
+            guide.GuideSurname=txtSurname.Text;
+            db.Guide.Add(guide);
+            db.SaveChanges();
+            var values = db.Guide.ToList();
+            dataGridView1.DataSource = values;
+            MessageBox.Show("Kişi Rehbere Başarıyla Eklendi.");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id=int.Parse(txtId.Text);
+            var removeValue=db.Guide.Find(id);
+            db.Guide.Remove(removeValue);
+            db.SaveChanges();
+            var values = db.Guide.ToList();
+            dataGridView1.DataSource = values;
+            MessageBox.Show("Kişi Başarıyla Silindi.");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var values = db.Guide.ToList();
+            dataGridView1.DataSource = values;
+            db.SaveChanges();
+        }
     }
 }
